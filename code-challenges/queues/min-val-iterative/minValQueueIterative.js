8175bc38cc4bd3872f8e1c46c9112b9a7fc4344c
Chris-Bortel <bortelcc19@gmail.com>
2021-01-11 13:36:48 -0800

    I have a solution

---
+++ code-challenges/queues/min-val-iterative/minValQueueIterative.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Queue = require('../index.js');
+
+let queue = new Queue();
+console.log(queue)
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(2);
+queue.enqueue(10);
+queue.enqueue(1);
+// console.log(JSON.stringify(queue,undefined,2));
+// console.log('PROBLEM DOMAIN: Given a Queue, Find the minimum value ... Iteratively.');
+
+
+let minVal = 0;
+console.log(minVal);
+let firstRemoved = queue.dequeue();
+if(firstRemoved >= minVal) {
+  minVal = firstRemoved;
+}
+console.log(minVal)
+
+while(queue.peek()){
+  let removed = queue.dequeue()
+  if(removed < minVal) {
+    minVal = removed;
+  }
+}
+console.log(minVal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
